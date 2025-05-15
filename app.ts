9b537b2102d4c5d94afc7cf6ac7c135ba5d30078
Jeremy Ma <jyaoma@gmail.com>
2025-05-15 09:22:44 -0500

    Fix issues in testing fetch endpoint

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 require("dotenv").config();
 import createError from 'http-errors';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
@@ -34,14 +34,16 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  console.error(err);
+
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.sendStatus(500);
+  // res.render('error');
 });
 
 module.exports = app;
