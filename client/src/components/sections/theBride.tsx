d201227b6ecfbd97cf915cf6d544b0433c30363f
Jeremy Ma <jyaoma@gmail.com>
2025-10-13 14:17:08 -0500

    Change picture for The Bride

--- client/src/components/sections/theBride.tsx
+++ client/src/components/sections/theBride.tsx
@@ -2,19 +2,23 @@ import React from 'react';
 import {LoaderContextActions, useLoaderDispatchContext} from "../../infra/loaderContext";
 import Slide from "../slide";
 
+import './theBride.css';
+
 const TheBride = () => {
     const loaderDispatch = useLoaderDispatchContext();
 
     return (
         <Slide className="the-bride" isTwoPane>
-            <img
-                src={require('../../images/retta5.png')}
-                alt="the bride"
-                className="two-pane-left the-bride-left"
-                onLoad={() => {
-                    loaderDispatch!({type: LoaderContextActions.INCREMENT_LOADED_ITEMS});
-                }}
-            />
+            <div className="two-pane-left">
+                <img
+                    src={require('../../images/page2.jpg')}
+                    alt="the bride"
+                    className="the-bride-left"
+                    onLoad={() => {
+                        loaderDispatch!({type: LoaderContextActions.INCREMENT_LOADED_ITEMS});
+                    }}
+                />
+            </div>
             <div className="two-pane-right the-bride-right">
                 <div className="pane-header">The Bride</div>
                 <div>Gabriella Claretta Dwiputri</div>
