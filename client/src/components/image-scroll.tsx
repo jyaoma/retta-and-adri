59401d12376438a7b6ea849a762613968e6ebdb2
Jeremy Ma <jyaoma@gmail.com>
2025-10-21 11:32:01 -0500

    Revision 20251021 - Add arrow to indicate scrollable

--- client/src/components/image-scroll.tsx
+++ client/src/components/image-scroll.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
 import './image-scroll.css';
 
@@ -9,20 +10,29 @@ type ImageScrollProps = {
     incrementLoadedItems: () => void;
 }
 
-const ImageScroll = (props: ImageScrollProps) => (
-    <div className="image-scroll">
-        <div className="image-scroll-inner">
-            {props.images.map((image) => (
-                <img
-                    key={image}
-                    src={image}
-                    alt={props.alt}
-                    className={props.className}
-                    onLoad={props.incrementLoadedItems}
-                />
-            ))}
-        </div>
-    </div>
-);
+const ImageScroll = (props: ImageScrollProps) => {
+    const [isTouched, setIsTouched] = useState(false);
+
+    return (
+        <>
+            <div className="image-scroll" onScroll={() => setIsTouched(true)}>
+                <div className="image-scroll-inner">
+                    {props.images.map((image) => (
+                        <img
+                            key={image}
+                            src={image}
+                            alt={props.alt}
+                            className={props.className}
+                            onLoad={props.incrementLoadedItems}
+                        />
+                    ))}
+                </div>
+            </div>
+            <div className={`image-scroll-arrow-container ${isTouched ? 'hidden' : ''}`}>
+                <ChevronRightIcon />
+            </div>
+        </>
+    );
+};
 
 export default ImageScroll;
